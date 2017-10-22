3141ce71e154888a118db687f40c969771f43e50
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-10-22 03:44:10 +0200

    Fix indentation and fix error when calling `setState`.

--- client/components/App.js
+++ client/components/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <main style={{textAlign: 'center'}}>
+        <main>
           <h1>arriven</h1>
           <h4>We make sure you&#39;ll be arriven at your destination.</h4>
           <SearchForm />
--- client/components/SearchForm.js
+++ client/components/SearchForm.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react';
 
 export default class SearchForm extends Component {
   constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.inputChange = this.inputChange.bind(this);
-    this.inputSubmit = this.inputSubmit.bind(this);
+    super(props)
+    this.state = {value: ''}
   }
 
-  inputChange(event) {
-alert(event.target.value);
-    this.setState({value: event.target.value});
+  inputChange = (event) => {
+    alert(event.target.value)
+    this.setState({value: event.target.value})
   }
 
-  inputSubmit(event) {
-alert(event.target.value);
-    event.preventDefault();
+  inputSubmit = (event) => {
+    alert(event.target.value)
+    event.preventDefault()
   }
 
   render() {
     return (
       <form onSubmit={this.inputSubmit}>
-        <input type="text" name="s" placeholder="Ziel suchen ..." value={this.state.value} onChange={this.inputChange}/>
+        <input type="text" placeholder="Ziel suchen ..." value={this.state.value} onChange={this.inputChange}/>
       </form>
     );
   }
