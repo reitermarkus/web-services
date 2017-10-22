8e43c2ea7f2ac829e2ba7346ac9a8885c9a26bc0
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-10-23 00:25:52 +0200

    Split webpack config into production and development.

---
+++ webpack.config.prod.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: './client/index.js',
+  output: {
+    filename: 'arriven.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader","css-loader","sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
