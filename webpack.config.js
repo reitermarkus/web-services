12bfeeb85daaff2a6f5ae8348b8ebe0f3bbd4af8
Markus Reiter <me@reitermark.us>
2017-10-22 04:03:30 +0200

    Add rules to `eslint` configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -40,10 +40,10 @@ module.exports = {
   },
   module: {
     loaders: [
-      { 
-        test: /\.js$/, 
-        loader: 'babel-loader', 
-        exclude: /node_modules/ 
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.js$/,
