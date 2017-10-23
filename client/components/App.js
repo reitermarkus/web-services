a739a3094c65debc04bea7f1ae01f41c48947ab6
Markus Reiter <me@reitermark.us>
2017-10-24 01:26:04 +0200

    Refactor header.

--- client/components/App.js
+++ client/components/App.js
@@ -11,7 +11,6 @@ export default class App extends Component {
       <div>
         <Header />
         <main>
-          <h1>arriven</h1>
           <h4>We make sure you&#39;ll be arriven at your destination.</h4>
           <SearchForm />
         </main>
