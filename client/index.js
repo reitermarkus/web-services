9639f0247f8aaf2d8957298e9f967b63c0e0c5fb
Markus Reiter <me@reitermark.us>
2017-10-25 01:31:08 +0200

    Extend `Number` prototype.

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import App from './components/App'
+import './prototypes'
 
 const render = (Component) => {
   ReactDOM.render(
