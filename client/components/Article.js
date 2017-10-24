fc219e3e9aa1bef7def6ab471e421825a3dc79c0
Markus Reiter <me@reitermark.us>
2017-10-25 01:42:04 +0200

    Refactor `Slider`.

--- client/components/Article.js
+++ client/components/Article.js
@@ -3,7 +3,12 @@ import Slider from './Slider'
 
 const Article = () => (
   <article>
-    <Slider />
+    <Slider images={[
+      'https://static.pexels.com/photos/414459/pexels-photo-414459.jpeg',
+      'https://static.pexels.com/photos/355241/pexels-photo-355241.jpeg',
+      'https://static.pexels.com/photos/161039/panorama-roppen-village-mountains-161039.jpeg',
+      'https://static.pexels.com/photos/414369/pexels-photo-414369.jpeg',
+    ]} />
   </article>
 )
 
--- client/components/Slider.js
+++ client/components/Slider.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class Slider extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      images : [
-        'https://static.pexels.com/photos/414459/pexels-photo-414459.jpeg',
-        'https://static.pexels.com/photos/355241/pexels-photo-355241.jpeg',
-        'https://static.pexels.com/photos/161039/panorama-roppen-village-mountains-161039.jpeg',
-        'https://static.pexels.com/photos/414369/pexels-photo-414369.jpeg',
-      ],
-      pos : 0,
+      pos: 0,
     }
+  }
 
-    this.consts = {
-      percent: 100,
-      ZERO: 0,
-      ONE: 1,
+  left = () => {
+    if (this.state.pos.positive) {
+      this.setState((prevState) => ({ pos: prevState.pos.decrement }))
+    }
+  }
+
+  right = () => {
+    if (this.state.pos < this.props.images.length.decrement) {
+      this.setState((prevState) => ({ pos: prevState.pos.increment }))
     }
   }
 
@@ -24,26 +25,30 @@ export default class Slider extends Component {
     return (
       <div className={'slider'}>
         <div className={'slider-container'}>
-          <div className={'img-container'} style={{transform: `translateX(-${this.state.pos * this.consts.percent}%)`}}>
-            {this.state.images.map((img, i) => {
+          <div className={'img-container'} style={{transform: `translateX(-${this.state.pos}00%)`}}>
+            {this.props.images.map((img, i) => {
               return <div key={i} style={{backgroundImage: `url(${img})`}}></div>
             })}
           </div>
           <div className={'circ-container'}>
-            {[...Array(this.state.images.length)].map((x, i) => {
+            {[...Array(this.props.images.length)].map((x, i) => {
               return <div key={i} onClick={() => { this.setState({pos: i}) }}>{(i) === this.state.pos ? '\u2689' : '\u26ac'}</div>
             })}
           </div>
           <div className={'btn-container'}>
-            <div className={'left-btn'} onClick={() =>
-            { this.state.pos > this.consts.ZERO ? this.setState(
-              (prevState) => ({ pos: prevState.pos - this.consts.ONE })) : null }}>&larr;</div>
-            <div className={'right-btn'} onClick={() =>
-            { this.state.pos < this.state.images.length - this.consts.ONE ?
-              this.setState((prevState) => ({ pos: prevState.pos + this.consts.ONE })) : null }}>&rarr;</div>
+            <div className={'left-btn'} onClick={this.left}>&larr;</div>
+            <div className={'right-btn'} onClick={this.right}>&rarr;</div>
           </div>
         </div>
       </div>
     )
   }
+}
+
+Slider.propTypes = {
+  images: PropTypes.array,
+}
+
+Slider.defaultProps = {
+  images: [],
 }
