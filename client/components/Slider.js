874e232fa8f7272acb4520885251a4a4b6fd582e
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-10-24 22:52:22 +0200

    make slider more react-like.

--- client/components/Slider.js
+++ client/components/Slider.js
@@ -3,68 +3,44 @@ import React, { Component } from 'react'
 export default class Slider extends Component {
   constructor(props) {
     super(props)
-    this.pos = 0
-  }
-
-  prevPicture = (event) => {
-    // Check image-limit
-    if (this.pos <= 0) {
-      return
+    this.state = {
+      images : [
+        'https://static.pexels.com/photos/414459/pexels-photo-414459.jpeg',
+        'https://static.pexels.com/photos/355241/pexels-photo-355241.jpeg',
+        'https://static.pexels.com/photos/161039/panorama-roppen-village-mountains-161039.jpeg',
+        'https://static.pexels.com/photos/414369/pexels-photo-414369.jpeg',
+      ],
+      pos : 0,
     }
 
-    // Move slider
-    this.pos -= 1
-    // Ugly, needs refactor
-    let slider = event.target.parentNode.parentNode.parentNode
-    let imgContainer = slider.querySelector('.img-container')
-    imgContainer.style.transform = 'translateX(-' + this.pos * 100 + '%)'
-
-    // Update slider-circles
-    let circles = slider.querySelectorAll('.circ-container > div')
-    circles[this.pos + 1].innerHTML = '&#9900;'
-    circles[this.pos].innerHTML = '&#9865;'
-  }
-
-  nextPicture = (event) => {
-    // Ugly, needs refactor
-    let slider = event.target.parentNode.parentNode.parentNode
-    let imgContainer = slider.querySelector('.img-container')
-    let images = imgContainer.querySelectorAll('div')
-
-    // Check image-limit
-    if (this.pos >= images.length - 1) {
-      return
+    this.consts = {
+      percent: 100,
+      ZERO: 0,
+      ONE: 1,
     }
-
-    // Move slider
-    this.pos += 1
-    imgContainer.style.transform = 'translateX(-' + this.pos * 100 + '%)'
-
-    // Update circles
-    let circles = slider.querySelectorAll('.circ-container > div')
-    circles[this.pos - 1].innerHTML = '&#9900;'
-    circles[this.pos].innerHTML = '&#9865;'
   }
 
   render() {
     return (
       <div className={'slider'}>
         <div className={'slider-container'}>
-          <div className={'img-container'}>
-            <div style={{'background-image': 'url(\'https://static.pexels.com/photos/414459/pexels-photo-414459.jpeg\')'}}></div>
-            <div style={{'background-image': 'url(\'https://static.pexels.com/photos/355241/pexels-photo-355241.jpeg\')'}}></div>
-            <div style={{'background-image': 'url(\'https://static.pexels.com/photos/161039/panorama-roppen-village-mountains-161039.jpeg\')'}}></div>
-            <div style={{'background-image': 'url(\'https://static.pexels.com/photos/414369/pexels-photo-414369.jpeg\')'}}></div>
+          <div className={'img-container'} style={{transform: `translateX(-${this.state.pos * this.consts.percent}%)`}}>
+            {this.state.images.map((img, i) => {
+              return <div key={i} style={{backgroundImage: `url(${img})`}}></div>
+            })}
           </div>
           <div className={'circ-container'}>
-            <div>&#9865;</div>
-            <div>&#9900;</div>
-            <div>&#9900;</div>
-            <div>&#9900;</div>
+            {[...Array(this.state.images.length)].map((x, i) => {
+              return <div key={i}>{(i) === this.state.pos ? '\u2689' : '\u26ac'}</div>
+            })}
           </div>
           <div className={'btn-container'}>
-            <div className={'left-btn'} onClick={(e) => { this.prevPicture(e) }}>&larr;</div>
-            <div className={'right-btn'} onClick={(e) => { this.nextPicture(e) }}>&rarr;</div>
+            <div className={'left-btn'} onClick={() =>
+            { this.state.pos >= this.consts.ZERO ? this.setState(
+              (prevState) => ({ pos: prevState.pos - this.consts.ONE })) : null }}>&larr;</div>
+            <div className={'right-btn'} onClick={() =>
+            { this.state.pos < this.state.images.length - this.consts.ONE ?
+              this.setState((prevState) => ({ pos: prevState.pos + this.consts.ONE })) : null }}>&rarr;</div>
           </div>
         </div>
       </div>
