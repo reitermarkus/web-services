a2c4c2bbeabf81b04fb0e970a09b997466a924c4
Markus Reiter <me@reitermark.us>
2017-10-26 03:20:14 +0200

    Add `Number.prototype.clamp`.

--- client/prototypes.js
+++ client/prototypes.js
@@ -27,3 +27,19 @@ Object.defineProperty(Number.prototype, 'zero', {
     return this === 0 /* eslint no-magic-numbers: 0 */
   },
 })
+
+Object.defineProperty(Number.prototype, 'increment', {
+  get: function increment() {
+    return this + 1 /* eslint no-magic-numbers: 0 */
+  },
+})
+
+Object.defineProperty(Number.prototype, 'decrement', {
+  get: function decrement() {
+    return this - 1 /* eslint no-magic-numbers: 0 */
+  },
+})
+
+Number.prototype.clamp = function(min, max) {
+  return this <= min ? min : this >= max ? max : this
+}
