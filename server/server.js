1fd40699abb4ee052d858953dd6c9e4987ce2233
Markus Reiter <me@reitermark.us>
2017-10-27 00:32:02 +0200

    Use React instead of a template.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
-import {Helmet} from 'react-helmet'
-import Template from './template'
+import { Helmet } from 'react-helmet'
 import App from '../client/components/App'
+
 const SUCCESS = 200
 
 const serverRenderer = () => {
   return (req, res) => {
-    const markup = ReactDOMServer.renderToString(
+    const markup = ReactDOMServer.renderToStaticMarkup(
       <StaticRouter location={ req.url } context={ {} }>
         <App />
       </StaticRouter>
@@ -16,10 +16,23 @@ const serverRenderer = () => {
 
     const helmet = Helmet.renderStatic()
 
-    res.status(SUCCESS).send(Template({
-      markup: markup,
-      helmet: helmet,
-    }))
+    res.status(SUCCESS).send(
+      ReactDOMServer.renderToStaticMarkup(
+        <html {...helmet.htmlAttributes.toComponent()}>
+          <head>
+            {helmet.title.toComponent()}
+            {helmet.meta.toComponent()}
+            {helmet.link.toComponent()}
+            {helmet.script.toComponent()}
+            {helmet.style.toComponent()}
+          </head>
+          <body {...helmet.bodyAttributes.toComponent()}>
+            <div id='root' dangerouslySetInnerHTML={{__html: markup}}/>
+            <script src='/arriven.bundle.js' async/>
+          </body>
+        </html>
+      )
+    )
   }
 }
 
