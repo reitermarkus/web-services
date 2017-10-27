d32656703c798a747368afaf282bf09567ecc8da
Markus Reiter <me@reitermark.us>
2017-10-27 19:54:38 +0200

    Add `dotenv` support.

--- client/components/App.js
+++ client/components/App.js
@@ -21,6 +21,7 @@ const App = () => (
         <div>
           <Header />
           <main>
+            OPENWEATHERMAP_API_KEY: {process.env.OPENWEATHERMAP_API_KEY}
             <h4>We make sure you&#39;ll be arriven at your destination.</h4>
             <SearchForm />
             <Article/>
