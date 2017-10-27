b90522815bab3bf16a598f2ccfc381e877411328
Markus Reiter <me@reitermark.us>
2017-10-27 18:49:42 +0200

    Fix typo.

--- client/components/App.js
+++ client/components/App.js
@@ -21,7 +21,7 @@ const App = () => (
         <div>
           <Header />
           <main>
-            <h4>We make sure you&#39;ll be arriveddn at your destination.</h4>
+            <h4>We make sure you&#39;ll be arriven at your destination.</h4>
             <SearchForm />
             <Article/>
           </main>
