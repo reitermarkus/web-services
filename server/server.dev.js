6f585d38382ec8cc66d2d8fea4468754457b143f
Markus Reiter <me@reitermark.us>
2017-10-30 02:19:18 +0100

    Allow CORS.

--- server/server.dev.js
+++ server/server.dev.js
@@ -8,6 +8,12 @@ const config = require('../webpack.config.dev')
 const compiler = webpack(config)
 const PORT = 3000
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(webpackDevMiddleware(compiler, {
   serverSideRender: true,
 }))
