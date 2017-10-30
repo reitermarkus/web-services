742fdf266910e22591d044e502a089c2bae0d823
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-10-30 02:51:26 +0100

    Serve assets.

---
+++ client/assets/server.prod.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const ServerRenderer = require(path.resolve(__dirname, 'server.js')).default
+const PORT = 3000
+
+app.use(express.static(path.resolve(__dirname, '../dist')))
+app.use(ServerRenderer())
+
+app.listen(PORT)
