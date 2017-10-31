576db253149ee517030979d657bcb6a7fe89b0d1
Markus Reiter <me@reitermark.us>
2017-10-31 04:54:39 +0100

    Use single line comments.

--- client/prototypes.js
+++ client/prototypes.js
@@ -1,42 +1,42 @@
 Object.defineProperty(Number.prototype, 'positive', {
   get: function positive() {
-    return this > 0 /* eslint no-magic-numbers: 0 */
+    return this > 0 // eslint-disable-line no-magic-numbers
   },
 })
 
 Object.defineProperty(Number.prototype, 'positiveOrZero', {
   get: function positiveOrZero() {
-    return this >= 0 /* eslint no-magic-numbers: 0 */
+    return this >= 0 // eslint-disable-line no-magic-numbers
   },
 })
 
 Object.defineProperty(Number.prototype, 'negative', {
   get: function negative() {
-    return this < 0 /* eslint no-magic-numbers: 0 */
+    return this < 0 // eslint-disable-line no-magic-numbers
   },
 })
 
 Object.defineProperty(Number.prototype, 'negativeOrZero', {
   get: function negativeOrZero() {
-    return this <= 0 /* eslint no-magic-numbers: 0 */
+    return this <= 0 // eslint-disable-line no-magic-numbers
   },
 })
 
 Object.defineProperty(Number.prototype, 'zero', {
   get: function zero() {
-    return this === 0 /* eslint no-magic-numbers: 0 */
+    return this === 0 // eslint-disable-line no-magic-numbers
   },
 })
 
 Object.defineProperty(Number.prototype, 'increment', {
   get: function increment() {
-    return this + 1 /* eslint no-magic-numbers: 0 */
+    return this + 1 // eslint-disable-line no-magic-numbers
   },
 })
 
 Object.defineProperty(Number.prototype, 'decrement', {
   get: function decrement() {
-    return this - 1 /* eslint no-magic-numbers: 0 */
+    return this - 1 // eslint-disable-line no-magic-numbers
   },
 })
 
