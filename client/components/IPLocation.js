93b88fb221ca68e5e9cb0a992ca55309706e4eff
Markus Reiter <me@reitermark.us>
2017-11-01 07:33:06 +0100

    Use native `geolocation` if available and add `onLocationChange` callback.

--- client/components/IPLocation.js
+++ client/components/IPLocation.js
@@ -6,35 +6,56 @@ export default class IPLocation extends Component {
     super(props)
 
     this.state = {
-      status: null,
-      country: null,
-      countryCode: null,
-      region: null,
-      regionName: null,
-      city: null,
-      zip: null,
-      lat: null,
-      lon: null,
-      timezone: null,
-      isp: null,
-      org: null,
-      as: null,
-      query: null,
+      initialized: false,
+      lat: 0,
+      lon: 0,
     }
   }
 
   componentDidMount() {
-    fetch(`http://ip-api.com/json/${this.props.ip}`)
+    fetch('https://geoip-db.com/json')
       .then(res => res.json())
-      .then(res => this.setState(res))
+      .then(res => {
+        if (!this.state.initialized) {
+          this.setLocation({
+            lat: res.latitude,
+            lon: res.longitude,
+          })
+        }
+      })
+
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.setLocation({
+          lat: pos.coords.latitude,
+          lon: pos.coords.longitude,
+        })
+      }, err => {
+        if (process.env.NODE_ENV === 'development') {
+          console.error(err) // eslint-disable-line no-console
+        }
+      })
+    }
+  }
+
+  setLocation({lat, lon}) {
+    this.setState({
+      initialized: true,
+      lat: lat,
+      lon: lon,
+    })
+
+    if (this.onLocationChange) {
+      this.props.onLocationChange({lat: lat, lon: lon})
+    }
   }
 
   render = () =>
-    `You are here: ${this.state.city} (${this.state.country}) [${this.state.lat}, ${this.state.lon}]`
+    `You are here: (${this.state.lat}, ${this.state.lon})`
 }
 
 IPLocation.propTypes = {
-  ip: PropTypes.string,
+  onLocationChange: PropTypes.func,
 }
 
 IPLocation.defaultProps = {
