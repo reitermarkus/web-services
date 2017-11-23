8845e252b28ff431a27e1d80847e9a8008b9fdae
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-11-23 22:23:26 +0100

    Save IP of user globally.

--- client/store.js
+++ client/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
 import { combineReducers } from 'redux'
 import imageReducer from './reducers/image-reducer'
+import locationReducer from './reducers/location-reducer'
 
 if (typeof window === 'undefined') {
   global.window = {}
@@ -12,6 +13,7 @@ delete window.__PRELOADED_STATE__
 
 const reducers = combineReducers({
   imageReducer: imageReducer,
+  locationReducer: locationReducer,
 })
 
 const store = createStore(reducers, preloadedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
