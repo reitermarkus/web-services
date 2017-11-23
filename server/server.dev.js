9ad1965f382f20a4337fdf952355ecd456035da7
Markus Reiter <me@reitermark.us>
2017-11-23 23:30:11 +0100

    Add SSL support to for using the `geolocation` API.

--- server/server.dev.js
+++ server/server.dev.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const http = require('http')
+const https = require('https')
 const app = express()
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
@@ -9,7 +11,16 @@ const compiler = webpack(config)
 const routes = require('./express-routes')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const PORT = 3000
+const fs = require('fs')
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+const SSL_PORT = process.env.SSL_PORT || 3043
+
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+}
 
 mongoose.connect('mongodb://localhost/arriven')
 const db = mongoose.connection
@@ -36,10 +47,20 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler.compilers.find(compiler => compiler.name === 'client')))
 app.use(webpackHotServerMiddleware(compiler))
 
-app.listen(PORT, (error) => {
+http.createServer(app).listen(PORT, (error) => {
   if (error) {
     console.error(error) // eslint-disable-line no-console
   } else {
     console.info(`Server started at http://127.0.0.1:${PORT}/`) // eslint-disable-line no-console
   }
 })
+
+https.createServer(sslOptions, app).listen(SSL_PORT, (error) => {
+  if (error) {
+    console.error(error) // eslint-disable-line no-console
+  } else {
+    console.info(`Server started at https://127.0.0.1:${SSL_PORT}/`) // eslint-disable-line no-console
+  }
+})
+
+
