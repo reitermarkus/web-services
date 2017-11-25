311f9407ace653f31fb5e228147833b386ee09a8
Markus Reiter <me@reitermark.us>
2017-11-25 22:52:21 +0100

    Refactor Fixer caching API.

--- server/schema/fixer.js
+++ server/schema/fixer.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const fixerSchema = new mongoose.Schema({
+const httpStatus = require('http-status-codes')
+const axios = require('axios')
+
+const schema = new Schema({
   base: {
     type: String,
     unique: false,
@@ -21,6 +25,35 @@ const fixerSchema = new mongoose.Schema({
   },
 })
 
-const fixer = mongoose.model('Fixer', fixerSchema)
+schema.static('ratesForBase', (req, res) => {
+  const base = req.params.base
+  const today = (new Date()).toISOString().split('T')[0]
 
-module.exports = fixer
+  mongoose.model('Fixer').findOne({
+    '$and' : [
+      {base: base},
+      {date: today},
+    ],
+  }, (err, result) => {
+    if (result === null || err) {
+      axios.get(`https://api.fixer.io/latest?base=${base}`)
+        .then(({ data }) => {
+          data.date = today
+
+          mongoose.model('Fixer').create(data, (err) => {
+            if (err) {
+              console.error(err) // eslint-disable-line no-console
+            }
+          })
+
+          res.status(httpStatus.OK)
+          res.json(data.rates)
+        })
+    } else {
+      res.status(httpStatus.OK)
+      res.json(result.rates)
+    }
+  })
+})
+
+module.exports = mongoose.model('Fixer', schema)
