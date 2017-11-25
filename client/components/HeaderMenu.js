2e1e9be329464585253fe39ec015cba1be582703
Markus Reiter <me@reitermark.us>
2017-11-25 21:56:53 +0100

    Refactor Pixabay caching API.

--- client/components/HeaderMenu.js
+++ client/components/HeaderMenu.js
@@ -4,7 +4,6 @@ const HeaderMenu = () =>
   <nav>
     <ul>
       <li><a href='/admin/location' target='_blank'>manage locations</a></li>
-      <li><a href='/api/pixabay/clear' target='_blank'>clear pixabay cache</a></li>
     </ul>
   </nav>
 
--- server/model/Pixabay.js
+++ server/model/Pixabay.js
@@ -1,65 +1,34 @@
 const httpStatus = require('http-status-codes')
 const axios = require('axios')
-const pix = require('../schema/pixabay')
+const PixabayModel = require('../schema/Pixabay')
 
 class Pixabay {
-  constructor(req, res) {
-    this.req = req
-    this.res = res
-    this.query = req.body.query
-    this.data = []
-  }
+  static find(req, res) {
+    const query = req.params.query
 
-  cache(data) {
-    pix.create(data, (err) => {
-      if (err) {
-        console.error(err) // eslint-disable-line no-console
-      }
-    })
-  }
-
-  static clear(res) {
-    pix.remove({}, (err) => {
-      if (err) {
-        res.status(httpStatus.OK).send('not ok')
-      } else {
-        res.status(httpStatus.OK).send('ok')
-      }
-    })
-  }
-
-  fetchFromApi(cb) {
-    axios.get(`https://pixabay.com/api/?q=${this.query}&image_type=photo&category=buildings&key=${process.env.PIXABAY_API_KEY}`)
-      .then(res => {
-        cb(res.data)
-      })
-  }
-
-  fetchFromCache(cb) {
-    pix.findOne({query: this.query}, cb)
-  }
-
-  find() {
-    this.fetchFromCache((err, result) => {
+    // Try fetching from database first.
+    PixabayModel.findOne({query: query}, (err, result) => {
       // Pixabay invalidates image-links after some time.
       // At the moment we assume it is about 24 hours. So we store the time, when we cached the entry.
       // If the time exceeds the given delta, we refetch data from Pixabay.
       const dayInMS = 86400000
 
       if (result === null || err || (Date.now() - result.time || 0) > dayInMS) {
-        this.fetchFromApi(res => {
-          let data = res
+        axios.get(`https://pixabay.com/api/?q=${query}&image_type=photo&category=buildings&key=${process.env.PIXABAY_API_KEY}`)
+          .then(({ data }) => {
+            data.query = query
+            data.time = Date.now()
 
-          data.query = this.query
-          data.time = Date.now()
-          this.data = data
+            PixabayModel.create(data, (err) => {
+              if (err) {
+                console.error(err) // eslint-disable-line no-console
+              }
+            })
 
-          this.cache(data)
-
-          this.res.status(httpStatus.OK).send(JSON.stringify(data))
-        })
+            res.status(httpStatus.OK).send(JSON.stringify(data))
+          })
       } else {
-        this.res.status(httpStatus.OK).send(JSON.stringify(result))
+        res.status(httpStatus.OK).send(JSON.stringify(result))
       }
     })
   }
