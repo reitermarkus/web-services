6d383c355d35660a2a81a1eda3c33f1a736829cf
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-12-27 22:43:53 +0100

    Add base component for authentication.

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
-import Logo from '../../arriven-logo.svg'
 import axios from 'axios'
+import AuthenticationSkeleton from './AuthenticationSkeleton'
 
 export default class Login extends Component {
   constructor(props) {
@@ -33,18 +33,13 @@ export default class Login extends Component {
     }
 
     return (
-      <div className='login-form'>
-        <div className='top'>
-          <Logo className='logo'/>
-        </div>
-        <div className='wrapper'>
-          <form>
-            <input type='text' value={this.state.email} onChange={(e) => { this.setState({email: e.target.value}) }} placeholder='email' />
-            <input type='password' value={this.state.password} onChange={(e) => { this.setState({password: e.target.value}) }} placeholder='password' />
-            <button type='button' onClick={this.login}>login</button>
-          </form>
-        </div>
-      </div>
+      <AuthenticationSkeleton height={'22em'}>
+        <form>
+          <input type='text' value={this.state.email} onChange={(e) => { this.setState({email: e.target.value}) }} placeholder='email' />
+          <input type='password' value={this.state.password} onChange={(e) => { this.setState({password: e.target.value}) }} placeholder='password' />
+          <button type='button' onClick={this.login}>login</button>
+        </form>
+      </AuthenticationSkeleton>
     )
   }
 }
