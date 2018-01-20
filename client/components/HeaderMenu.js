1570294cdf3c326e1df3fd11e6382c679bc3482c
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-01-20 13:50:55 +0100

    Use react router for redirecting.

--- client/components/HeaderMenu.js
+++ client/components/HeaderMenu.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { deleteToken } from '../jwt'
+import { Redirect } from 'react-router-dom'
 import userAction from '../actions/user-action'
 import store from '../store'
 
 class HeaderMenu extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      redirect: false,
+    }
   }
 
   removeUserData = () => {
@@ -14,15 +19,23 @@ class HeaderMenu extends Component {
     deleteToken()
   }
 
-  render = () =>
-    <nav>
-      <ul>
-        {this.props.user ?
-          <li><a onClick={() => this.removeUserData()} target='_blank'>logout</a></li> :
-          <li><a href='/login' target='_blank'>login</a></li> }
-        <li><a href='/admin/location' target='_blank'>manage locations</a></li>
-      </ul>
-    </nav>
+  render = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/login' />
+    }
+
+    return (
+      <nav>
+        <ul>
+          {this.props.user ?
+            <li><a onClick={() => this.removeUserData()} target='_blank'>logout</a></li> :
+            <li><a onClick={() => this.setState({redirect: true})} target='_blank'>login</a></li> }
+          <li><a href='/admin/location' target='_blank'>manage locations</a></li>
+        </ul>
+      </nav>
+    )
+  }
+
 }
 
 const mapStateToProps = (store) => {
